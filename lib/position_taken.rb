ce3cfe9e9c54c4b43b162e86177666738914e5c7
Sean Murphy <filmmakersean@fastmail.net>
2020-03-09 18:25:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  return !(board[index] == " " || board[index] == "" || board[index] == nil)
-  return (board[index] == "X" || board[index] == "O")
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+  (board[index] == "X" || board[index] == "O")
   end
